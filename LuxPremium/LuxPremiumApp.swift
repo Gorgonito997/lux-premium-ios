092020b480d6f56d8c25f71f4f2d9b7307e29c25
Diego Ca(n)sado <diegocm505@gmail.com>
2026-04-14 13:17:59 +0200

    Prueba Conexión a firestore

--- LuxPremium/LuxPremiumApp.swift
+++ LuxPremium/LuxPremiumApp.swift
@@ -4,11 +4,23 @@
 //
 //  Created by admin on 4/14/26.
 //
-
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct LuxPremiumApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
