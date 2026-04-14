1dcff25524042c58e287055e8b3a5ef0f045ae7d
Diego CM <diegocm505@gmail.com>
2026-04-14 10:56:47 +0100

    Initial iOS project

---
+++ LuxPremium/LuxPremiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuxPremiumApp.swift
+//  LuxPremium
+//
+//  Created by admin on 4/14/26.
+//
+
+import SwiftUI
+
+@main
+struct LuxPremiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LuxPremiumTests/LuxPremiumTests.swift
@@ -0,0 +1,17 @@
+//
+//  LuxPremiumTests.swift
+//  LuxPremiumTests
+//
+//  Created by admin on 4/14/26.
+//
+
+import Testing
+@testable import LuxPremium
+
+struct LuxPremiumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
